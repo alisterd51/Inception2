8b99aee0a07cdb50c2b67cf155f8fb12933e1f4d
Antoine CLARMAN <anclarma@e1r2p8.42.fr>
2021-06-09 21:28:40 +0200

    base ok

--- srcs/mariadb/entrypoint-mariadb.sh
+++ srcs/mariadb/entrypoint-mariadb.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+mysql_install_db
+service mysql start
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+service mysql stop
+mysqld -u root
