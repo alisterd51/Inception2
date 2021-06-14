f9a1af51d4171d00624553df68d1dc57435b995e
Antoine CLARMAN <anclarma@e1r3p12.42.fr>
2021-06-14 17:04:00 +0200

    maj entrypoint-mariadb

--- srcs/mariadb/entrypoint-mariadb.sh
+++ srcs/mariadb/entrypoint-mariadb.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-mysql_install_db
+#mysql_install_db
+#service mysql start
+#mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+#mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+#mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#mysql -e "FLUSH PRIVILEGES;"
+#mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /base.sql
+#service mysql stop
+#mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql --defaults-file=/etc/mysql/my.cnf > /dev/null
+#exec mysqld --user=root --console
+mysql_install_db --user=mysql --datadir="/var/lib/mysql"
 service mysql start
 mysqladmin -u root password $MYSQL_ROOT_PASSWORD
 mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -8,4 +19,4 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MY
 mysql -e "FLUSH PRIVILEGES;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /base.sql
 service mysql stop
-exec mysqld -u root
+exec mysqld_safe --datadir="/var/lib/mysql"
